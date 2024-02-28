b90b00f4cab9f5b44a0662ac125212e968e18e2a
aby-c329 <alexacampos329@gmail.com>
2024-02-27 19:40:58 -0600

    calculadora basica

--- main.py
+++ main.py
@@ -6,3 +6,50 @@ def saludar():
 nombre = saludar()
 print(f'Hola {nombre}, ¿cómo estás?')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def oper_arismet():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    operacion = input("Ingrese la operación deseada (suma, resta, multiplicacion, division): ")
+
+    if operacion == "suma":
+        resultado = num1 + num2
+    elif operacion == "resta":
+        resultado = num1 - num2
+    elif operacion == "multiplicacion":
+        resultado = num1 * num2
+    elif operacion == "division":
+        if num2 == 0:
+            print('ERROR')
+            return None
+        resultado = num1 / num2
+    else:
+        print('ingrese una operacion valida')
+        return None
+
+
+    print(f"El resultado de {num1} {operacion} {num2} es: {resultado}")
+    return resultado
+
+
+resultado = oper_arismet()
